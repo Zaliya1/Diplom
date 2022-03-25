d41a5827c4419a5d2f53144912e9438c0bdd79da
Zaliya Validova <z.validova@yandex.ru>
2022-03-25 23:01:19 +0500

    Диплом

--- src/modules/slider2.js
+++ src/modules/slider2.js
@@ -8,7 +8,7 @@ const sliders = () => {
       breakpoints: {
         1200: {
           slidesPerView: 2,
-          spaceBetween: 10,
+          
           grid: {
             rows: 1,
             fill: 'row'
@@ -16,7 +16,7 @@ const sliders = () => {
         },
         576: {
           slidesPerView: 1,
-          spaceBetween: 10,
+          
           grid: {
             rows: 2,
             fill: 'row'
